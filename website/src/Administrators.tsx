cb9d3493c6604a224227836a72e7c8a50279c04f
Giuseppetm <gdeambo@yahoo.it>
2020-12-18 19:23:05 +0100

    Admin section update

--- website/src/Administrators.tsx
+++ website/src/Administrators.tsx
@@ -20,16 +20,17 @@ const Administrators = () => {
             <Text style={{ fontSize: FontSizes.size16 }}>
                 <p className="mb-5">
                     Ogni corso di laurea e i suoi relativi gruppi telegram hanno come riferimento degli amministratori che possono
-                    essere contattati in caso di necessità. Qui è possibile trovare la lista degli admin di ogni corso di laurea.
+                    essere contattati in caso di necessità. Qui è possibile trovare la lista degli admin di ogni corso di laurea: quelli evidenziati
+                    sono i tutor di riferimento per le matricole attuali.
                 </p>
             </Text>
-
             <Text style={{ fontSize: FontSizes.size14 }}>
                 <div className="row justify-content-center">
                     <div className="col-sm-6 col-md-4 col-lg-2 mb-5">
                         <h6 style={{ fontWeight: 500 }}>Informatica</h6>
                         <p className="mb-1"><a href="https://t.me/giuseppetm">@giuseppetm</a></p>
                         <p className="mb-1"><a href="https://t.me/davidebusolin">@davidebusolin</a></p>
+                        <p className="mb-1"><a href="https://t.me/alesiasommariva" className="tutor">@alesiasommariva</a></p>
                         <p className="mb-1"><a href="https://t.me/MrBrionix">@MrBrionix</a></p>
                         <p className="mb-1"><a href="https://t.me/mattia_oldani">@mattia_oldani</a></p>
                         <p className="mb-1"><a href="https://t.me/acetimarco">@acetimarco</a></p>
@@ -44,21 +45,21 @@ const Administrators = () => {
                     </div>
                     <div className="col-sm-6 col-md-4 col-lg-2 mb-5">
                         <h6 style={{ fontWeight: 500 }}>Informatica musicale</h6>
-                        <p className="mb-1"><a href="https://t.me/giolake">@giolake</a></p>
+                        <p className="mb-1"><a href="https://t.me/giolake" className="tutor">@giolake</a></p>
                         <p className="mb-1"><a href="https://t.me/robertopinotti">@robertopinotti</a></p>
                         <p className="mb-1"><a href="https://t.me/elmalakomar">@elmalakomar</a></p>
                         <p className="mb-1"><a href="https://t.me/giuseppetm">@giuseppetm</a></p>
                     </div>
                     <div className="col-sm-6 col-md-4 col-lg-2 mb-5">
                         <h6 style={{ fontWeight: 500 }}>Informatica per la comunicazione digitale</h6>
-                        <p className="mb-1"><a href="https://t.me/robertopinotti">@robertopinotti</a></p>
+                        <p className="mb-1"><a href="https://t.me/robertopinotti" className="tutor">@robertopinotti</a></p>
                         <p className="mb-1"><a href="https://t.me/giolake">@giolake</a></p>
                         <p className="mb-1"><a href="https://t.me/elmalakomar">@elmalakomar</a></p>
                         <p className="mb-1"><a href="https://t.me/giuseppetm">@giuseppetm</a></p>
                     </div>
                     <div className="col-sm-6 col-md-4 col-lg-2 mb-5">
                         <h6 style={{ fontWeight: 500 }}>Sicurezza dei sistemi e delle reti informatiche</h6>
-                        <p className="mb-1"><a href="https://t.me/elmalakomar">@elmalakomar</a></p>
+                        <p className="mb-1"><a href="https://t.me/elmalakomar" className="tutor">@elmalakomar</a></p>
                         <p className="mb-1"><a href="https://t.me/robertopinotti">@robertopinotti</a></p>
                         <p className="mb-1"><a href="https://t.me/giolake">@giolake</a></p>
                         <p className="mb-1"><a href="https://t.me/burzum00">@burzum00</a></p>
