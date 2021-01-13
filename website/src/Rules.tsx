c8ffc7e71d9627c4e81b8949b378016d357b017d
Giuseppetm <gdeambo@yahoo.it>
2021-01-13 14:20:07 +0100

    Some adjustments

--- website/src/Rules.tsx
+++ website/src/Rules.tsx
@@ -7,8 +7,12 @@ import { Container } from 'react-bootstrap';
 
 const bold = {
     fontWeight: 600,
-    color: '#4f6bed',
+    color: '#0078d4',
     fontSize: FontSizes.size14
+};
+
+const rules = {
+    fontWeight: 600
 };
 
 const Rules = () => {
@@ -23,7 +27,7 @@ const Rules = () => {
                 </p>
             </Text>
 
-            <div style={{ 'marginLeft': '15%', 'marginRight': '15%'}}>
+            <div style={{ 'marginLeft': '5%', 'marginRight': '5%'}}>
                 <Text style={{ fontSize: FontSizes.size12 }}>
 
 
@@ -46,7 +50,7 @@ const Rules = () => {
                     <p className="mb-3">Di seguito è possibile trovare il regolamento.</p>
 
                     
-                    <div id="accordion" className="mr-3 ml-3">
+                    <div id="accordion">
 
                             {/* Regole riguardo domande e faq */}
                             <div className="card mb-1">
@@ -62,13 +66,13 @@ const Rules = () => {
                                 <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                                     <div className="card-body">
                                         
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Domanda con risposta indicata sul sito del docente</p>
+                                        <p className="mb-1" style={rules}>Domanda con risposta indicata sul sito del docente</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             L'utente riceve un warn con successiva indicazione su dove trovare l'informazione richiesta.
 	                                        Dopo 3 warn l'utente viene mutato per un giorno, a discrezione degli amministratori.
                                         </p>
 
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Domanda riguardo informazioni banali e già chieste precedentemente nella chat</p>
+                                        <p className="mb-1" style={rules}>Domanda riguardo informazioni banali e già chieste precedentemente nella chat</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             Nessuna penalità se non sono presenti le FAQ del gruppo del relativo corso, altrimenti vale la regola precedente, ovvero: l'utente riceve un warn con successiva indicazione della presenza delle FAQ.
                                             Dopo 3 warn l'utente viene mutato per un giorno, a discrezione degli amministratori.
@@ -93,21 +97,21 @@ const Rules = () => {
                                 <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                                     <div className="card-body">
                                         
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Bestemmie e linguaggio vivace</p>
+                                        <p className="mb-1" style={rules}>Bestemmie e linguaggio vivace</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             Le bestemmie sono vietate, pena warn per quelle innocenti, e mute o ban nei casi peggiori, a discrezione degli amministratori. 
                                             Il linguaggio vivace è permesso finchè non diventa spam: in quel caso l'utente verrà warnato o mutato per un certo periodo di tempo, 
                                             sempre a discrezione degli amministratori.
                                         </p>
                                         
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Insulti e offese</p>
+                                        <p className="mb-1" style={rules}>Insulti e offese</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             Sono entrambi vietati, pena warn per gli insulti / offese leggeri, mute o ban per quelli più pesanti, a discrezione degli amministratori.
 	                                        Una nota riguardo i docenti: si può benissimo criticare (nei limiti del normale) ma non si accettano assolutamente insulti verso di essi. 
                                             Nell'ultimo caso specificato l'utente verrà warnato o mutato, e nei casi peggiori bannato, a discrezione degli amministratori.
                                         </p>
 
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Spam e offtopic</p>
+                                        <p className="mb-1" style={rules}>Spam e offtopic</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             Lo spam è vietato.
                                             Le conversazioni offtopic sono vietate nei gruppi specifici dei corsi e anche in quelli generali degli anni accademici.
@@ -132,7 +136,7 @@ const Rules = () => {
                                 <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                                     <div className="card-body">
                                         
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Nome e username</p>
+                                        <p className="mb-1" style={rules}>Nome e username</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             Gli utenti devono essere identificabili sul network, quindi è obbligatorio utilizzare un nome che sia chiaro, come
                                             il nome di persona o un breve appellativo. Per quanto riguarda l'username è caldamente consigliato (ma non obbligatorio)
@@ -140,7 +144,7 @@ const Rules = () => {
                                             del network.
                                         </p>
                                         
-                                        <p className="mb-1" style={{ fontWeight: 600 }}>Politica</p>
+                                        <p className="mb-1" style={rules}>Politica</p>
                                         <p className="mb-3 ml-3 mr-3">
                                             Il network e i gruppi telegram sono nati come spazio politicamente neutrale e il più possibile accogliente, a disposizione degli studenti. 
                                             Non sono di proprietà o vanto di alcuna lista. Chiunque manderà messaggi di propaganda elettorale e quant'altro verrà istantaneamente 
