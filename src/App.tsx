2b595ce0dd57b7958fe000368b4ac1479c7b0fe8
Giuseppetm <gdeambo@yahoo.it>
2021-01-27 21:40:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 /*
 TODO LIST
--1 Sezione dei corsi di laurea, ci mettiamo un pulsantino per switchare tra gruppi e informazioni generali
--2 Sempre nella sezione dei corsi devi poter selezionare il dipartimento e poi i vari corsi di laurea
-- Pulsante per switchare tra ssri e ssri online. Usa un basic toggle con callout visualizzato tramite hover
-- Sezione del sito sulla lista di rappresentanti (magari usa i componenti person)
-- Sezione riguardo faq sulla didattica a distanza (aspetta comunicazioni ufficiali)
-- Sezione del sito con manifesti e informazioni globali sui cdl, e anche la guida sul come disiscriversi dalle liste mail
-- Homepage con news prelevate dal sito di unimia
-- Pulsante per switchare al tema scuro
-- Trova un modo per passare da italiano a inglese per eventuali studenti stranieri
 - Hash commit nel footer
 - Costruire la gerarchia del network, fai un file .psd così lo tieni aggiornato facilmente
-- Futura ristrutturazione: card con solo icone + hover sopra che mostra tooltip
 */
 
 import React from "react";
