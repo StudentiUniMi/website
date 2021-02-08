7f7afb549bed6becb991e671f81269327fc001c2
Giuseppetm <gdeambo@yahoo.it>
2021-02-08 21:19:31 +0100

    Update AdminsList.tsx

--- src/components/AdminsList.tsx
+++ src/components/AdminsList.tsx
@@ -2,6 +2,7 @@ import Admin from '../models/Admin';
 import { IPersonaSharedProps, Persona, PersonaSize } from 'office-ui-fabric-react/lib/Persona';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import { Link } from 'office-ui-fabric-react';
 
 interface Props {
     data: Admin[]
@@ -16,7 +17,7 @@ const AdminListView = (props: Props) => {
         <Row className="admin-list text-center">
             {props.data.map(x => 
                 <Col xl={3} lg={3} md={4} sm={6} xs={12} className="mb-3 col-persona">
-                    <Persona {...defaultPersona} text={`@${x.username ?? ""}`} size={PersonaSize.size40} />
+                    <Persona {...defaultPersona} onRenderPrimaryText={(t) => <Link href={`t.me/${x.username}`}>{`@${x.username ?? ""}`}</Link>} text={`@${x.username}` ?? ""} size={PersonaSize.size40} />
                 </Col>
             )}
         </Row>
