f58f0cc5e8128165d0113851e934740cd049eaa0
Giuseppetm <gdeambo@yahoo.it>
2021-02-28 12:07:43 +0100

    SCSS Support + Consolas font added

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './index.scss';
 import MainView from './views/MainView';
 
 ReactDOM.render(
