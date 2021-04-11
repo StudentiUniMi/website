5ae86ae07a501532bbc7e17b1b0e5bb7a27cd3a6
Giuseppetm <gdeambo@yahoo.it>
2021-04-11 13:23:15 +0200

    Services section text fixed

--- src/views/ServicesView.tsx
+++ src/views/ServicesView.tsx
@@ -4,8 +4,8 @@ import { Container } from 'react-bootstrap';
 import { useTheme } from '@fluentui/react-theme-provider';
 import { DocumentCardPreview, IDocumentCardPreviewProps } from 'office-ui-fabric-react/lib/DocumentCard';
 import { Card, ICardTokens } from '@uifabric/react-cards';
-import { FontWeights, ITextStyles } from 'office-ui-fabric-react';
 import { Icon, Link } from 'office-ui-fabric-react';
+import { semibold } from '../fonts';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { getServices } from '../services/Requests';
@@ -14,7 +14,10 @@ import { redirectToLink } from '../services/Utils';
 
 const Services = () => {
     var theme = useTheme();
+    const services = getServices();
     const iconStyle = { color: theme.palette.themePrimary, fontSize: FontSizes.size24 };
+    const cardTokens: ICardTokens = { childrenMargin: 12 };
+
     let cardProps = (iconName?: string, backgroundColor?: string): IDocumentCardPreviewProps => {
         return {
             previewImages: [ 
@@ -30,31 +33,19 @@ const Services = () => {
         }
     };
     
-    const siteTextStyles: ITextStyles = {
-        root: { fontWeight: FontWeights.semibold, fontSize: FontSizes.size14, color: theme.palette.themePrimary },
-    };
-    
-    const descriptionTextStyles: ITextStyles = {
-        root: { fontWeight: FontWeights.regular, fontSize: FontSizes.size12 },
-    };
-    
-    const cardTokens: ICardTokens = { childrenMargin: 12 };
-    
-    let services = getServices();
-
     return (
         <Container className="services text-center">
             <div className="mb-3">
-                <h5 style={{fontWeight: 400}}>
+                <Text variant="large">
                     Siete stanchi di dover andare a spulciare miriadi di pagine che neanche caricano alla ricerca di strumenti e servizi universitari?
-                    Abbiamo realizzato una pagina per centralizzarli tutti.
-                </h5>
+                    Abbiamo realizzato una pagina per centralizzarli tutti!
+                </Text>
             </div>
 
             <Icon iconName="ChevronDownMed" className="mb-2" style={iconStyle} />
 
             <div className="mb-4">
-                <Text style={{ fontSize: FontSizes.size18 }}>
+                <Text variant="medium">
                     Se pensi che debba essere aggiunto qualcosa scrivi pure sul <Link href="https://t.me/joinchat/VswKeAblS2nrfXME" target="_blank">gruppo principale</Link>.
                 </Text>
             </div>
@@ -68,13 +59,8 @@ const Services = () => {
                                     <DocumentCardPreview {...cardProps(x.icon, x.color)}/>
                                 </Card.Item>
                                 <Card.Section>
-                                    <Text variant="small" styles={siteTextStyles}>{x.name}</Text>
-                                    <Text styles={descriptionTextStyles}>{x.description}</Text>
-                                    {/*
-                                    <Text variant="small" styles={helpfulTextStyles}>
-                                    Is this recommendation helpful?
-                                    </Text>
-                                    */}
+                                    <Text variant="medium" style={{color: theme.palette.themePrimary}} styles={semibold}>{x.name}</Text>
+                                    <Text variant="small">{x.description}</Text>
                                 </Card.Section>
                             </Card>
                         </Col>
