6499737a039671b356a512e83076986eb4f12dfa
Giuseppe Del Campo <52317197+Giuseppetm@users.noreply.github.com>
2021-06-30 21:54:44 +0200

    Added chips to cards and enhanced their description (#193)

--- src/components/ExtraGroup.tsx
+++ src/components/ExtraGroup.tsx
@@ -1,24 +1,27 @@
-import { Text } from 'office-ui-fabric-react';
+import { Text, Icon } from 'office-ui-fabric-react';
 import { initializeIcons } from '@uifabric/icons';
 import { FontWeights, ITextStyles, Persona } from '@fluentui/react';
 import { Card, ICardTokens } from "@uifabric/react-cards";
 import ExtraGroup from '../models/ExtraGroup'
-// import { useTheme } from '@fluentui/react-theme-provider';
+import { useTheme } from '@fluentui/react-theme-provider';
 import { ActionButton } from '@fluentui/react/lib/Button';
 import { IIconProps } from '@fluentui/react';
 import { redirectToLink } from '../services/Utils';
+import Chip from '@material-ui/core/Chip';
 
 initializeIcons();
 
 interface Props { data: ExtraGroup };
 
 const ExtraGroupView = (props: Props) => {
+    const theme = useTheme();
     var data = props.data;
     const helpfulTextStyles: ITextStyles = {
         root: {
             fontWeight: FontWeights.regular,
         },
     };
+    const descriptionTextStyles: ITextStyles = { root: { fontWeight: FontWeights.semibold } };
     const cardTokens: ICardTokens = { childrenMargin: 12 };
     const telegramGroupIcon: IIconProps = { iconName: 'Send' };
 
@@ -28,8 +31,11 @@ const ExtraGroupView = (props: Props) => {
                 { data.image === "" ? <Persona text={data.name} /> : <Persona text={data.name} imageUrl={ process.env.PUBLIC_URL + '/extra_groups_images/' + data.image } /> }
             </Card.Item>
             <Card.Section>
+                <Text styles={descriptionTextStyles}>
+                    <Chip label="Gruppo extra" size="small" style={{ color: theme.palette.white, backgroundColor: theme.palette.orangeLighter }} className="m-1" />
+                </Text>
                 <Text variant="small" styles={helpfulTextStyles} className="mb-2">
-                    {data.description}
+                    <Icon iconName="Info" className="homeIcon" /> {data.description}
                 </Text>
 
                 {
