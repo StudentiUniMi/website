ad7500e78e69ac3d84853e8be0fa47919fe2ddc0
Giuseppetm <gdeambo@yahoo.it>
2021-08-22 18:13:35 +0200

    Update HomeView.tsx

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -26,7 +26,7 @@ import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { redirectToLink } from '../services/dist/Utils';
+import { redirectToLink } from '../services/Utils';
 
 SwiperCore.use([Pagination, Navigation, Autoplay]);
 initializeIcons();
