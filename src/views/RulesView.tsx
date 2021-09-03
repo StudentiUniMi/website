1b4c17178c8b593da6fb0396637a989f91ce686a
Giuseppetm <gdeambo@yahoo.it>
2021-09-03 14:53:37 +0200

    Rules section restructured

--- src/views/RulesView.tsx
+++ src/views/RulesView.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { FontSizes } from '@fluentui/theme';
-import { Text, Icon, Callout, Link } from 'office-ui-fabric-react';
+import { Text, Callout, Link } from 'office-ui-fabric-react';
 import { Container } from 'react-bootstrap';
 import { semibold } from '../services/fonts';
 import { useTheme } from '@fluentui/react-theme-provider';
-import { DefaultButton, DirectionalHint, IIconProps, mergeStyleSets, Separator } from "@fluentui/react";
+import { DefaultButton, DirectionalHint, IIconProps, mergeStyleSets } from "@fluentui/react";
 import { useBoolean, useId } from '@fluentui/react-hooks';
 import { getRules } from '../services/Requests'; 
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { Image } from 'office-ui-fabric-react/lib/Image';
 import Rule from '../models/Rule';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
@@ -19,10 +21,11 @@ const Rules = () => {
     const theme = useTheme();
     const locale = LocalizationService.strings();
     var language: string = LocalizationService.getLanguage();
-    const iconStyle = { color: theme.palette.themePrimary, fontSize: FontSizes.size24 };
     const icon: IIconProps = { iconName: 'QandA' };
 
     const rulesData: Rule[] = getRules();
+
+    const imageProperties = { display: 'inline-block', width: '90%' };
 
     const [isCalloutVisible, { toggle: toggleIsCalloutVisible }] = useBoolean(false);
     const buttonId = useId('callout-button');
@@ -37,77 +40,94 @@ const Rules = () => {
         button: {
             width: 'auto',
             height: 'auto',
-            marginLeft: 10,
-            marginRight: 10
+            borderRadius: 3
         },
     });
 
     return (
-        <Container className="rules text-center">
-            <div className="mb-4">
-                <div className="mb-3">
-                    <Text variant="large">
-                        {locale.rules.text1}
-                    </Text>
-                </div>
+        <div className="rules">
+            <div className="pt-5 pb-5 mb-4" style={{ backgroundColor: theme.palette.themePrimary }}>
+                <Container>
 
-                <div className="mb-3">
-                    <DefaultButton
-                        id={buttonId}
-                        onClick={toggleIsCalloutVisible}
-                        text={locale.rules.question}
-                        className={styles.button}
-                        iconProps={icon}
-                        theme={theme}
-                    />
-                    {isCalloutVisible && (
-                        <Callout
-                            className={styles.callout}
-                            role="alertdialog"
-                            gapSpace={3}
-                            target={`#${buttonId}`}
-                            onDismiss={toggleIsCalloutVisible}
-                            directionalHint={DirectionalHint.bottomCenter}
-                            setInitialFocus
-                        >
-                            <Text block variant="medium" className="mb-1">{locale.rules.answer.text1}</Text>
-                            <Text block variant="small"><JsxParser bindings={{ theme: theme }} components={{ Text, Link }} jsx={locale.rules.answer.text2} /></Text>
-                        </Callout>
-                    )}
-                </div>
+                    <Row>
+                        <Col lg={4} className="text-center">
+                            <div style={{ marginLeft: 'auto', marginRight: 'auto', maxWidth: 300 }}>
+                                <Image id="logo" className="mb-2" src={process.env.PUBLIC_URL + '/other/rules.png'} style={imageProperties} />
+                            </div>
+                        </Col>
 
-                <Icon iconName="ChevronDownMed" className="mb-3" style={iconStyle} />
+                        <Col lg={8} className="mb-2">
+                            <div className="mb-1">
+                                <Text variant="xLargePlus" style={{color: theme.palette.white}}>Il regolamento dei nostri gruppi Telegram ci rende più innovativi e produttivi.</Text>
+                            </div>
 
-                <div className="mb-3">
-                    <Separator theme={theme}><Text variant="large" styles={semibold}>{locale.rules.text2}</Text></Separator>
-                </div>
+                            <div className="mb-2">
+                                <Text variant="large" style={{ color: theme.palette.white }}>Si consiglia di leggere tutte le regole di cui è composto prima di usare uno qualsiasi di essi.</Text>
+                            </div>
 
-                <div className="mb-3">
-                {
-                    rulesData.map((x, i) => {
-                        return (
-                            <Accordion style={{ backgroundColor: theme.palette.white, color: theme.palette.black, boxShadow: theme.effects.elevation8, marginRight: 10, marginLeft: 10 }} key={i}>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon style={{ color: theme.palette.black }} />}
-                                    aria-controls="panel1a-content"
-                                    id="panel1a-header"
-                                >
-                                    <Text variant="medium" style={{ color: theme.palette.themePrimary }} styles={semibold}>{x.title![language]}</Text>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    <Text variant="medium">
-                                        <JsxParser bindings={{ theme: theme, semibold: semibold }} components={{ Text, Link }} jsx={x.description![language]} />
-                                    </Text>
-                                </AccordionDetails>
-                            </Accordion>
-                        )
-                    })
-                }
-                </div>
+                            <div>
+                                <DefaultButton
+                                    id={buttonId}
+                                    onClick={toggleIsCalloutVisible}
+                                    text={locale.rules.question}
+                                    className={styles.button}
+                                    iconProps={icon}
+                                    theme={theme}
+                                />
+                                {isCalloutVisible && (
+                                    <Callout
+                                        className={styles.callout}
+                                        role="alertdialog"
+                                        gapSpace={3}
+                                        target={`#${buttonId}`}
+                                        onDismiss={toggleIsCalloutVisible}
+                                        directionalHint={DirectionalHint.bottomCenter}
+                                        setInitialFocus
+                                    >
+                                        <Text block variant="medium" className="mb-1">{locale.rules.answer.text1}</Text>
+                                        <Text block variant="small"><JsxParser bindings={{ theme: theme }} components={{ Text, Link }} jsx={locale.rules.answer.text2} /></Text>
+                                    </Callout>
+                                )}
+                            </div>
+                        </Col>
+                    </Row>
 
+                </Container>
             </div>
+            
+            <Container className="rules text-center">
+                <div className="mb-4">
+                    <div className="mb-4 text-center">
+                        {/*<div className="mb-1"><Text variant="medium" styles={semibold} style={{textTransform: 'uppercase', color: theme.palette.themePrimary}}>Gruppi degli insegnamenti</Text></div>*/}
+                        <Text variant="xLarge">Regolamento dei gruppi Telegram</Text>
+                    </div>
+                </div>
 
-        </Container>
+                    <div className="mb-3">
+                    {
+                        rulesData.map((x, i) => {
+                            return (
+                                <Accordion style={{ backgroundColor: theme.palette.white, color: theme.palette.black, boxShadow: theme.effects.elevation8, marginRight: 10, marginLeft: 10 }} key={i}>
+                                    <AccordionSummary
+                                        expandIcon={<ExpandMoreIcon style={{ color: theme.palette.black }} />}
+                                        aria-controls="panel1a-content"
+                                        id="panel1a-header"
+                                    >
+                                        <Text variant="medium" style={{ color: theme.palette.themePrimary }} styles={semibold}>{x.title![language]}</Text>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <Text variant="medium">
+                                            <JsxParser bindings={{ theme: theme, semibold: semibold }} components={{ Text, Link }} jsx={x.description![language]} />
+                                        </Text>
+                                    </AccordionDetails>
+                                </Accordion>
+                            )
+                        })
+                    }
+                    </div>
+
+            </Container>
+        </div>
     )
 }
 
