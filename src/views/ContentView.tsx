a62f23d5a0773d43d00f3b9d52535a8fa6d35158
Giuseppetm <gdeambo@yahoo.it>
2021-10-13 20:50:27 +0200

    Added og properties to contentview elements and keywords

--- src/views/ContentView.tsx
+++ src/views/ContentView.tsx
@@ -25,7 +25,12 @@ const ContentView = () => {
                         <meta charSet="utf-8" />
                         <title>{locale?.helmet.homepage.title}</title>
                         <meta name="description" content={locale?.helmet.homepage.description} />
+                        <meta name="keywords" content="Network StudentiUniMi, Studenti UniMi, Studenti Unimi, StudentiUniMi, Network statale informatica, Gruppi telegram unimi, Gruppi unimi, Siti web corsi unimi, Faq corsi unimi, Wiki Unimi, Network Studenti Unimi" />
                         <link rel="canonical" hrefLang={language} href="https://studentiunimi.it/home/" />
+                        <meta property="og:title" content={locale?.helmet.homepage.title} />
+                        <meta property="og:description" content={locale?.helmet.homepage.description} />
+                        <meta property="og:type" content="website" />
+                        <meta property="og:url" content="https://studentiunimi.it/home/" />
                     </Helmet>
                     <HomeView/>
                 </>
@@ -42,7 +47,12 @@ const ContentView = () => {
                     <meta charSet="utf-8" />
                     <title>{locale?.helmet.services.title}</title>
                     <meta name="description" content={locale?.helmet.services.description} />
+                    <meta name="keywords" content="Unimia StudentiUnimi, Servizi StudentiUnimi, Studenti Unimi, StudentiUnimi, Network StudentiUnimi, Network Studenti Unimi, Servizi StudentiUnimi, Servizi Studenti Unimi, StudentiUnimi Services, Network StudentiUnimi Services" />
                     <link rel="canonical" hrefLang={language} href="https://studentiunimi.it/services/" />
+                    <meta property="og:title" content={locale?.helmet.services.title} />
+                    <meta property="og:description" content={locale?.helmet.services.description} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://studentiunimi.it/services/" />
                 </Helmet>
                     <ServicesView/>
                 </>
@@ -53,7 +63,12 @@ const ContentView = () => {
                     <meta charSet="utf-8" />
                     <title>{locale?.helmet.rules.title}</title>
                     <meta name="description" content={locale?.helmet.rules.description} />
+                    <meta name="keywords" content="Network StudentiUnimi regolamento gruppi, StudentiUnimi regolamento gruppi, Network StudentiUnimi rules, StudentiUnimi rules, Group rules network studentiunimi, Group rules studentiunimi, Studenti Unimi, StudentiUnimi, Network StudentiUnimi, Network Studenti Unimi" />
                     <link rel="canonical" hrefLang={language} href="https://studentiunimi.it/rules/" />
+                    <meta property="og:title" content={locale?.helmet.rules.title} />
+                    <meta property="og:description" content={locale?.helmet.rules.description} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://studentiunimi.it/rules/" />
                 </Helmet>
                     <RulesView/>
                 </>
@@ -64,7 +79,12 @@ const ContentView = () => {
                     <meta charSet="utf-8" />
                     <title>{locale?.helmet.university.title}</title>
                     <meta name="description" content={locale?.helmet.university.description} />
+                    <meta name="keywords" content="Network StudentiUnimi ateneo, StudentiUnimi ateneo, Network StudentiUnimi ateneo, StudentiUnimi ateneo, Network StudentiUnimi rappresentanti, StudentiUnimi rappresentanti, Network StudentiUnimi rappresentanti, StudentiUnimi rappresentanti, Studenti Unimi, StudentiUnimi, Network StudentiUnimi, Network Studenti Unimi" />
                     <link rel="canonical" hrefLang={language} href="https://studentiunimi.it/representatives/" />
+                    <meta property="og:title" content={locale?.helmet.university.title} />
+                    <meta property="og:description" content={locale?.helmet.university.description} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://studentiunimi.it/representatives/" />
                 </Helmet>
                     <UniversityView/>
                 </>
@@ -74,8 +94,13 @@ const ContentView = () => {
                 <Helmet>
                     <meta charSet="utf-8" />
                     <title>{locale?.helmet.organization.title}</title>
+                    <meta name="keywords" content="Network StudentiUnimi chi siamo?, StudentiUnimi chi siamo?, Network StudentiUnimi chi siamo, StudentiUnimi chi siamo, Network StudentiUnimi who we are, StudentiUnimi organization, Network StudentiUnimi organization, StudentiUnimi who we are, Studenti Unimi, StudentiUnimi, Network StudentiUnimi, Network Studenti Unimi" />
                     <meta name="description" content={locale?.helmet.organization.description} />
                     <link rel="canonical" hrefLang={language} href="https://studentiunimi.it/organization/" />
+                    <meta property="og:title" content={locale?.helmet.organization.title} />
+                    <meta property="og:description" content={locale?.helmet.organization.description} />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://studentiunimi.it/organization/" />
                 </Helmet>
                     <OrganizationView/>
                 </>
