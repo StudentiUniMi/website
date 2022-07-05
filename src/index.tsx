717dedc3216e34cab36ad6552224113e8a79b22a
Giuseppetm <giuseppe.delcampo@outlook.com>
2022-07-05 21:05:02 +0200

    React snap usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import "swiper/swiper.min.css";
 import "swiper/components/pagination/pagination.min.css";
@@ -7,9 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import MainView from './views/MainView';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <MainView />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement!.hasChildNodes()) {
+  ReactDOM.hydrate(<MainView />, rootElement);
+} else {
+  ReactDOM.render(<MainView />, rootElement);
+}
