82bb3c0126ceb11c222328bbdce663b63745531b
Giuseppetm <giuseppe.delcampo@outlook.com>
2022-09-02 12:20:59 +0200

    Html prefix now really works

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Stylesheet, InjectionMode } from '@uifabric/merge-styles';
 import { resetIds } from '@uifabric/utilities';
-import Document, { DocumentContext } from 'next/document';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 
 // Do this in file scope to initialize the stylesheet before Fluent UI React components are imported.
 const stylesheet = Stylesheet.getInstance();
@@ -22,5 +22,17 @@ export default class MyDocument extends Document {
             ...initialProps,
             styles: [initialProps.styles, <style key="fluentui-css" dangerouslySetInnerHTML={{ __html: stylesheet.getRules(true) }} />],
         }
+    }
+
+    render() {
+        return (
+            <Html prefix="og: http://ogp.me/ns#">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 };
