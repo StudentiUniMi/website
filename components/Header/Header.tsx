6d0ed5817024173dba1f116faa669b0463ce1a09
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2023-08-06 12:11:15 +0200

    React-headroom usage for sticky header behaviour
    Huge improvement in header behaviour

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,19 @@
-import { FontSizes, Text, Image, FontWeights, ITextStyles, Link, useTheme } from '@fluentui/react';
-import { useEffect, useRef } from 'react';
+import { 
+  FontSizes, 
+  Text,
+  Image, 
+  FontWeights, 
+  ITextStyles, 
+  Link, 
+  useTheme 
+} from '@fluentui/react';
 import HeaderMenu from './HeaderMenu';
+import Headroom from 'react-headroom';
 
 const Header = () => {
   var theme = useTheme();
+
+  const headerZIndex = 2;
 
   const logoProperties = {
     width: 25,
@@ -14,7 +24,7 @@ const Header = () => {
   const logoFileName = 'unimi150.png';
 
   const headerStyle = {
-    zIndex: 2,
+    zIndex: headerZIndex,
     backgroundColor: theme.palette.white,
     borderBottom: '1px solid',
     borderColor: theme.palette.neutralQuaternary,
@@ -29,87 +39,40 @@ const Header = () => {
     }
   };
 
-  const headerRef = useRef<HTMLHeadingElement>(null);
-  const prevScrollPos = useRef(0);
-  const prevScrollDirection = useRef("none");
-
-  const debounce = (func: any, delay: number) => {
-    let timeout: any;
-    return (...args: any) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        func(...args);
-      }, delay);
-    };
-  };
-
-  // Handle scroll logic
-  useEffect(() => {
-    window.addEventListener("scroll", debounce(() => {
-      window.requestAnimationFrame(() => {
-        const currentScrollPos = window.pageYOffset;
-        let currentScrollDirection = "none";
-        if (prevScrollPos.current > currentScrollPos) {
-          currentScrollDirection = "up";
-        } else if (prevScrollPos.current < currentScrollPos) {
-          currentScrollDirection = "down";
-        }
-        if (
-          prevScrollDirection.current === "down" &&
-          currentScrollDirection === "up"
-        ) {
-          headerRef.current?.classList.remove("hidden");
-          headerRef.current?.classList.add("shown");
-        } else if (
-          prevScrollDirection.current === "up" &&
-          currentScrollDirection === "down" &&
-          currentScrollPos > 200
-        ) {
-          headerRef.current?.classList.remove("shown");
-          headerRef.current?.classList.add("hidden");
-        } else if (
-          prevScrollDirection.current === "down" &&
-          currentScrollDirection === "down" &&
-          currentScrollPos > 200
-        ) {
-          headerRef.current?.classList.remove("shown");
-          headerRef.current?.classList.add("hidden");
-        }
-        prevScrollPos.current = currentScrollPos;
-        prevScrollDirection.current = currentScrollDirection;
-      });
-    }, 10));
-  }, []);
-
   return (
-    <header ref={headerRef} style={headerStyle} className="header shown">
+    <>
+      {/* @ts-ignore */} 
+      <Headroom style={{ zIndex: headerZIndex }}>
+        <header style={headerStyle} className="header">
 
-      <div className="d-flex flex-row" style={{ marginRight: 15, marginLeft: 15, marginBottom: 0 }}>
+          <div className="d-flex flex-row" style={{ marginRight: 15, marginLeft: 15, marginBottom: 0 }}>
 
-        <div style={{ maxWidth: 250, paddingLeft: 0, paddingRight: 0, display: 'flex', alignItems: 'center' }}>
-          <div className="d-flex align-items-center" style={{ width: 250, gap: 10 }}>
-            <Link href="http://studentiunimi.it/">
-              <Image
-                id="logo"
-                alt="Logo"
-                src={'/logo/' + logoFileName}
-                style={logoProperties}
-              />
-            </Link>
+            <div style={{ maxWidth: 250, paddingLeft: 0, paddingRight: 0, display: 'flex', alignItems: 'center' }}>
+              <div className="d-flex align-items-center" style={{ width: 250, gap: 10 }}>
+                <Link href="http://studentiunimi.it/">
+                  <Image
+                    id="logo"
+                    alt="Logo"
+                    src={'/logo/' + logoFileName}
+                    style={logoProperties}
+                  />
+                </Link>
 
-            <Link href="http://studentiunimi.it/" className="text-decoration-none">
-              <Text styles={titleStyle}>Network StudentiUniMi</Text>
-            </Link>
+                <Link href="http://studentiunimi.it/" className="text-decoration-none">
+                  <Text styles={titleStyle}>Network StudentiUniMi</Text>
+                </Link>
+              </div>
+            </div>
+
+            <div style={{ paddingLeft: 0, paddingRight: 0, flexGrow: 1, overflowX: 'hidden', overflowY: 'hidden' }}>
+              <HeaderMenu />
+            </div>
+
           </div>
-        </div>
 
-        <div style={{ paddingLeft: 0, paddingRight: 0, flexGrow: 1, overflowX: 'hidden', overflowY: 'hidden' }}>
-          <HeaderMenu />
-        </div>
-
-      </div>
-
-    </header>
+        </header>
+      </Headroom>
+    </>
   )
 };
 
