dbb4f468bbe9ce22dcf6cddd0e00ffe82058e5f7
Giuseppe Del Campo <giuseppe.delcampo@outlook.com>
2025-07-12 21:53:07 +0200

    Rollback document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,54 +1,37 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from 'next/document';
-import { Stylesheet, resetIds } from '@fluentui/react';
+import { Stylesheet, InjectionMode, resetIds } from '@fluentui/react';
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 
 const stylesheet = Stylesheet.getInstance();
 
-class MyDocument extends Document<{ styleTags: string; serializedStylesheet: string }> {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps & {
-    styleTags: string;
-    serializedStylesheet: string;
-  }> {
-    resetIds();
-    const initialProps = await Document.getInitialProps(ctx);
+stylesheet.setConfig({
+    injectionMode: InjectionMode.none,
+    namespace: 'server'
+});
 
-    return {
-      ...initialProps,
-      styleTags: stylesheet.getRules(true),
-      serializedStylesheet: stylesheet.serialize(),
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        stylesheet.reset();
+        resetIds();
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return {
+            ...initialProps,
+            styles: [ initialProps.styles, <style key="fluentui-css" dangerouslySetInnerHTML={{ __html: stylesheet.getRules(true) }} /> ]
+        }
     };
-  }
 
-  render() {
-    return (
-      <Html prefix="og: http://ogp.me/ns#" lang={"it"} translate="no">
-        <Head>
-          <style
-            type="text/css"
-            dangerouslySetInnerHTML={{ __html: this.props.styleTags }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.FabricConfig = { serializedStylesheet: ${JSON.stringify(
-                this.props.serializedStylesheet
-              )} };`,
-            }}
-          />
-          <link rel="stylesheet" href="/styles/fabric.min.css" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
+    render() {
+        return (
+            <Html prefix="og: http://ogp.me/ns#" lang={"it"} translate="no">
+                <Head>
+                  <link rel="stylesheet" href="/styles/fabric.min.css" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    };
+};
